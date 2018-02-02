a89ed430ea10b068196cbb63f07e79957981fdd6
Daniel Sheppard <dsio.business@gmail.com>
2018-02-02 02:42:21 -0600

    Fixing server connection issues

--- UnionDraw.js
+++ UnionDraw.js
@@ -10,7 +10,7 @@ var msgManager;
 var UPC = net.user1.orbiter.UPC;
 // The ID of the room users will join in order to draw together
 var roomID = "union";
-var url = "192.30.252.154";
+var url = "probstudios.com";
 // A hash of client attribute names used in this application. Each client sets a
 // "thickness" attribute and a "color" attribute, specify the thickness and
 // color of the current line being drawn.
