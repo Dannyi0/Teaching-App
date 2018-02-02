df594a76e6b137505540cb39ce23298213e1b8ed
Daniel Sheppard <dsio.business@gmail.com>
2018-02-01 23:18:44 -0600

    Initial Commit

---
+++ server.js
@@ -0,0 +1,4 @@
+
+
+
+console.log("Server is good to go, boss!");
---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup(){
+    createCanvas(400,400);
+}
+
+function draw(){
+    noStroke();
+    fill(20);
+    ellipse(mouseX, mouseY, 15, 15);
+}
