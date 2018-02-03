221b4057fb4ab7ae74a4a2d3936712241edb87de
Daniel Sheppard <dsio.business@gmail.com>
2018-02-02 23:27:09 -0600

    Adding basic teacher/student fuctionality

--- UnionDraw.js
+++ UnionDraw.js
@@ -13,16 +13,6 @@ var roomID = "";
 //Server to connect to
 var url = "tryunion.com";
 //Firebase variables
-var config = {
-  apiKey: "AIzaSyBnsWtr9YU5afQxxNhcqVL-NiXWD3-aYi0",
-  authDomain: "teaching-app-1.firebaseapp.com",
-  databaseURL: "https://teaching-app-1.firebaseio.com",
-  projectId: "teaching-app-1",
-  storageBucket: "teaching-app-1.appspot.com",
-  messagingSenderId: "690061668715"
-};
-firebase.initializeApp(config);
-
 var auth = firebase.auth();
 var userStatus; // 10 = student-only account, 11 = teacher & student account
 // A hash of client attribute names used in this application. Each client sets a
